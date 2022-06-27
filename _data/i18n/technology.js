db0daf3cc0c4dd795b82434a12d71bcda8710458
Federica Bernaschina <federicabernaschina@gmail.com>
2022-06-27 13:02:44 +0200

    technology translation

--- _data/i18n/technology.js
+++ _data/i18n/technology.js
@@ -1,94 +1,94 @@
 module.exports = {
     title: {
         'it': 'Tecnologie informatiche',
-        'en': ''
+        'en': 'Information Technology'
     },
     subtitle: {
         'it': 'Cosa proponiamo',
-        'en': ''
+        'en': 'What we offer'
     },
     description: {
         'it': 'Al fianco in ogni settore, accompagniamo i nostri clienti verso l’industria 4.0 e il rinnovamento strategico del proprio business, al fine di sfruttare al meglio le potenzialità offerte dalla trasformazione digitale.',
-        'en': ''
+        'en': 'Alongside in every sector, we accompany our customers towards industry 4.0 and the strategic renewal of their business, in order to make the most of the potential offered by digital transformation.'
     },
     discoverMoreCTA: {
         'it': 'Scopri i settori',
-        'en': ''
+        'en': 'Find out sectors'
     },
     slider: {
         title: {
             'it': 'I nostri servizi',
-            'en': ''
+            'en': 'Our services'
         },
         description: {
             'it': 'Sviluppiamo piattaforme multisettoriali per la gestione e l’integrità dei dati attraverso l’utilizzo di tecnologie Internet-of-Things (IoT) e Blockchain.',
-            'en': ''
+            'en': 'We develop multi-sector platforms for data management and integrity through the use of Internet-of-Thing (IoT) and Blockchain technologies.'
         },
         item01: {
             title: {
                 'it': 'Notarizzazione dei dati',
-                'en': ''
+                'en': 'Data notarization'
             },
             bulletPoints: {
                 item01: {
                     'it': '“Notarizzare” significa registrare digitalmente un qualsiasi documento in un registro distribuito (Blockchain) creando un file unico ed immutabile nel tempo.',
-                    'en': ''
+                    'en': '-Digitally registering any document in a distributed ledger (Blockchain), creating a single file unchangeable over time.'
                 },
                 item02: {
                     'it': 'Certificazione autenticità dei dati mediante firma digitale.',
-                    'en': ''
+                    'en': 'Data authenticity certification by digital signature.'
                 },
                 item03: {
                     'it': 'Tracciamento dei beni industriali nel modo più semplice ed efficiente. Gestione della relativa documentazione.',
-                    'en': ''
+                    'en': 'Tracking of industrial assets in the simplest and most efficient way. Management of related documentation.'
                 },
                 item04: {
                     'it': 'Gestione, creazione di report e registri storici delle manutenzioni svolte.',
-                    'en': ''
+                    'en': 'Management, creation of reports and historical records of the maintenance carried out.'
                 },
             }
         },
         item02: {
             title: {
                 'it': 'Gestione integrità dei dati',
-                'en': ''
+                'en': 'Data integrity management'
             },
             bulletPoints: {
                 item01: {
                     'it': 'Tracciamento dei beni industriali e gestione della relativa documentazione.',
-                    'en': ''
+                    'en': 'Tracking of industrial assets and management of related documentation'
                 },
                 item02: {
                     'it': 'Informazioni, monitoraggio e certificazioni d’uso dei beni industriali (es: ore di funzionamento, allarmi, anomalie, ecc.).',
-                    'en': ''
+                    'en': 'Information, monitoring and certifications of use of industrial goods (e.g. hours of operation, anomalies, alarms, etc.)'
                 },
                 item03: {
                     'it': 'Creazione di report in tempo reale direttamente dagli impianti e dalle macchine, certificando i tempi di attività, fermi macchina, allarmi, anomalie, temperature, ecc.',
-                    'en': ''
+                    'en': '-Real-time report creation directly from plants and machines, certifying uptime, machine downtime, alarms, anomalies, temperatures, etc.'
                 }
             }
         },
         item03: {
             title: {
                 'it': 'Trasformazione digitale 4.0',
-                'en': ''
+                'en': 'Digital transformation 4.0'
             },
             bulletPoints: {
                 item01: {
                     'it': 'Analisi e mappatura sistema informativo e integrità dati (AS-IS).',
-                    'en': ''
+                    'en': 'Analysis and mapping of the information system and data integrity (AS-IS)'
                 },
                 item02: {
                     'it': 'Sviluppo e configurazione acquisizione dati basto sul sistema Trusted IoT.',
-                    'en': ''
+                    'en': '-Development and configuration of data acquisition based on the Trusted IoT system.'
                 },
                 item03: {
                     'it': 'Realizzazione piattaforme web-app per certificazione dati in Blockchain.',
-                    'en': ''
+                    'en': 'Creation of web-app platforms for Blockchain data certification.'
                 },
                 item04: {
                     'it': 'Collaudo delle soluzioni sviluppate e formazione sul loro corretto utilizzo.',
-                    'en': ''
+                    'en': 'Testing of the solutions developed and training on their correct use'
                 }
             }
         }
@@ -96,54 +96,54 @@ module.exports = {
     contactForm: {
         title: {
             'it': 'Non esitare a contattarci',
-            'en': ''
+            'en': 'Do not hesitate to contact us'
         },
         subtitle: {
             'it': 'Hai un progetto in mente?',
-            'en': ''
+            'en': 'Do you have a project in mind?'
         },
         fullNameField: {
             label: {
                 'it': 'Nome e Cognome',
-                'en': ''
+                'en': 'First Name and Last Name'
             },
             errorMessage: {
                 'it': 'Inserisci il tuo Nome e Cognome!',
-                'en': ''
+                'en': 'Please enter your First Name and Last Name!'
             }
         },
         emailField: {
             label: {
                 'it': 'Email',
-                'en': ''
+                'en': 'Email'
             },
             errorMessage: {
                 'it': 'Inserisci la tua email!',
-                'en': ''
+                'en': 'Please enter your email!'
             }
         },
         messageField: {
             label: {
                 'it': 'Messaggio',
-                'en': ''
+                'en': 'Message'
             },
             errorMessage: {
                 'it': 'inserisci il tuo messaggio!',
-                'en': ''
+                'en': 'Please enter your message!'
             }
         },
         buttonCTA: {
             'it': 'Invia richiesta',
-            'en': ''
+            'en': 'Send request'
         },
         thankYouMessage: {
             'it': 'Grazie per averci contattato! Un membro del nostro team si metterà in contatto con te il prima possibile.',
-            'en': ''
+            'en': 'Thank you for contacting us! A member from our team will be reaching out to you soon.'
         },
         errorPopup: {
             failedEmailSending: {
                 'it': 'L\'invio dell\'email è fallito!',
-                'en': ''
+                'en': 'There was an error submitting the request.'
             }
         }
     }
