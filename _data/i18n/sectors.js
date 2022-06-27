179f041c1b24c86ed1d6f0e30f0a6121319a5816
Fabio Micheletti <fabio.micheletti@forge.srl>
2022-06-27 10:46:23 +0200

    Added sectors translations

--- _data/i18n/sectors.js
+++ _data/i18n/sectors.js
@@ -24,51 +24,88 @@ module.exports = {
     },
     item02:{
         title:{
-            'it': '',
+            'it': 'Manutenzione, tracciabilità e ispezione',
             'en': ''
         },
         paragraph:{
-            'it': '',
+            'it': '<ul>' +
+                '<li>Monitoraggio e certificazione di: allarmi, aggiornamenti, condizioni ambientali, tempi di servizio, attività di manutenzione e ispezioni, arresti macchine e sistemi antincendio.</li>' +
+                '<li>Creazione di report verificabili; digitalizzazione dei processi; miglioramento dell\'efficienza e dell\'affidabilità.</li>' +
+                '<li>Tracciamento ed evidenza di tutte le operazioni di carico e scarico, stato merce e ricevimenti. Monitoraggio e prova inalterabile dello stato di avanzamento dei lavori.</li>' +
+                '</ul>',
             'en': ''
         }
     },
     item03:{
         title:{
-            'it': '',
+            'it': 'Connettività e cybersecurity',
             'en': ''
         },
         paragraph:{
-            'it': '',
+            'it': '<ul>' +
+                '<li>Prova verificabile delle comunicazioni e dello stato di servizio in ogni momento.</li>' +
+                '<li>Certificazione di immagini satellitari.</li>' +
+                '<li>Certificazione cloud e server.</li>' +
+                '</ul>',
             'en': ''
         }
     },
     item04:{
         title:{
-            'it': '',
+            'it': 'Sicurezza',
             'en': ''
         },
         paragraph:{
-            'it': '',
+            'it': '<ul>' +
+                '<li>Negli ambienti industriali e infrastrutturali è indispensabile avere visibilità sullo stato di tutti i sistemi di sicurezza, come:\n<ul>' +
+                '<li>prova dell’evento per lo stato del sistema, allarmi ed incidenti;</li>' +
+                '<li>prova del lavoro per la manutenzione e le ispezioni;</li>' +
+                '<li>implementazione rapida della blockchain senza la necessità di una piattaforma specifica.</li>' +
+                '</ul>' +
+                '</li>' +
+                '<li>Tracciabilità dello stato della flotta aziendale in ogni momento, con una cronologia certificata delle attività. Assicurazione della massima qualità e trasparenza nel servizio di trasporto.</li>' +
+                '</ul>',
             'en': ''
         }
     },
     item05:{
         title:{
-            'it': '',
+            'it': 'Assicurativo e finanziario',
             'en': ''
         },
         paragraph:{
-            'it': '',
+            'it': '<ul>' +
+                '<li>Promozione di nuovi progetti e miglioramento di quelli esistenti aggiungendo strumenti innovativi e di rapida implementazione.</li>' +
+                '<li>Creazione di una cronologia certificata degli eventi soggetti a verifiche. Tracciamento, autenticità ed evidenza delle operazioni che sono soggette ai controlli assicurativi.</li>' +
+                '</ul>',
             'en': ''
         }
     },
     item06:{
         title:{
-            'it': '',
+            'it': 'Beni di valore',
             'en': ''
         },
         paragraph:{
-            'it': '',
+            'it': '<ul>' +
+                '<li>Tracciabilità delle materie prime e dei prodotti lungo la filiera, proteggendoli dalle contraffazioni.</li>' +
+                '<li>Certificazione di unicità dei singoli capi all\'interno di una collezione.</li>' +
+                '<li>Prova di autenticità di un bene prezioso, e relativa verifica di origine, dell’artista e altro. Esso può essere utilizzato come supporto fisico per NFT (Non-Fungible-Token) consentendo il commercio e il trasferimento di proprietà.</li>' +
+                '</ul>',
+            'en': ''
+        }
+    },
+    contactCTA: {
+        paragraph: {
+            'it': 'Sei pronto per iniziare?',
+            'en': ''
+        },
+        title: {
+            'it': 'Porta il tuo <span class="text-primary">Business</span> al livello superiore!',
+            'en': ''
+        },
+        buttonCTA: {
+            'it': 'Contattaci',
             'en': ''
         }
     }
