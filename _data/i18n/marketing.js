65ed79fe2482c96d4c229e0dc27a4f0e56357b6a
Fabio Micheletti <fabio.micheletti@forge.srl>
2022-06-27 11:13:50 +0200

    Added marketing translations

--- _data/i18n/marketing.js
+++ _data/i18n/marketing.js
@@ -1,6 +1,146 @@
 module.exports = {
     title: {
-        'it': 'Noi, l\'agenzia digitale per il tuo business',
-        'en': 'We are the digital agency for your business'
+        'it': 'Marketing e comunicazione',
+        'en': ''
+    },
+    subtitle: {
+        'it': 'Cosa proponiamoCosa proponiamo',
+        'en': ''
+    },
+    description: {
+        'it': 'Ti supportiamo per semplificare la tua operatività attraverso un\'assistenza puntuale ed un supporto professionale in tutte le fase del tuo progetto, offrendoti servizi di comunicazione aziendale e marketing strategico su misura.',
+        'en': ''
+    },
+    slider: {
+        title: {
+            'it': 'I nostri servizi',
+            'en': ''
+        },
+        description: {
+            'it': 'Forniamo servizi e consulenza in marketing e comunicazione aziendale attraverso un portafoglio di soluzioni integrate, accompagnando le aziende durante tutto il processo di ridefinizione e sviluppo del business aziendale.',
+            'en': ''
+        },
+        item01: {
+            title: {
+                'it': 'Comunicazione digitale e tradizionale',
+                'en': ''
+            },
+            bulletPoints: {
+                item01: {
+                    'it': 'Gestione social media (LinkedIn, Instagram, Facebook, TikTok, ecc.)',
+                    'en': ''
+                },
+                item02: {
+                    'it': 'Sviluppo siti web, mobile app, UX/UI design',
+                    'en': ''
+                },
+                item03: {
+                    'it': 'Presentazioni aziendali e di business',
+                    'en': ''
+                },
+                item04: {
+                    'it': 'Pubblicità e promozione',
+                    'en': ''
+                },
+            }
+        },
+        item02: {
+            title: {
+                'it': 'Creazione contenuti',
+                'en': ''
+            },
+            bulletPoints: {
+                item01: {
+                    'it': 'Infografiche digitali, motion graphics, caroselli, illustrazioni',
+                    'en': ''
+                },
+                item02: {
+                    'it': 'Produzione e post-produzione immagini',
+                    'en': ''
+                },
+                item03: {
+                    'it': 'Produzione e post-produzione video, sottotitoli, speakeraggio, montaggio finale',
+                    'en': ''
+                },
+                item04: {
+                    'it': 'Preparazione didascalie, storytelling, copywriting',
+                    'en': ''
+                },
+            }
+        },
+        item03: {
+            title: {
+                'it': 'Marketing strategico',
+                'en': ''
+            },
+            bulletPoints: {
+                item01: {
+                    'it': 'Brand strategy (analisi e studio del marchio e del modello di business)',
+                    'en': ''
+                },
+                item02: {
+                    'it': 'Digital marketing strategy (strategia dei social media, SEO/SEM, segmentazione clienti)',
+                    'en': ''
+                },
+                item03: {
+                    'it': 'Strategic selling (strategia dei canali e del processo di vendita)',
+                    'en': ''
+                }
+            }
+        }
+    },
+    contactForm: {
+        title: {
+            'it': 'Non esitare a contattarci',
+            'en': ''
+        },
+        subtitle: {
+            'it': 'Hai un progetto in mente?',
+            'en': ''
+        },
+        fullNameField: {
+            label: {
+                'it': 'Nome e Cognome',
+                'en': ''
+            },
+            errorMessage: {
+                'it': 'Inserisci il tuo Nome e Cognome!',
+                'en': ''
+            }
+        },
+        emailField: {
+            label: {
+                'it': 'Email',
+                'en': ''
+            },
+            errorMessage: {
+                'it': 'Inserisci la tua email!',
+                'en': ''
+            }
+        },
+        messageField: {
+            label: {
+                'it': 'Messaggio',
+                'en': ''
+            },
+            errorMessage: {
+                'it': 'inserisci il tuo messaggio!',
+                'en': ''
+            }
+        },
+        buttonCTA: {
+            'it': 'Invia richiesta',
+            'en': ''
+        },
+        thankYouMessage: {
+            'it': 'Grazie per averci contattato! Un membro del nostro team si metterà in contatto con te il prima possibile.',
+            'en': ''
+        },
+        errorPopup: {
+            failedEmailSending: {
+                'it': 'L\'invio dell\'email è fallito!',
+                'en': ''
+            }
+        }
     }
 }
