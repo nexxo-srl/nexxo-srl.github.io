1cc06312ccf82d84c1b261ca95ba56db565c77e8
Federica Bernaschina <federicabernaschina@gmail.com>
2022-06-27 12:15:49 +0200

    Update marketing.js

--- _data/i18n/marketing.js
+++ _data/i18n/marketing.js
@@ -1,6 +1,146 @@
 module.exports = {
     title: {
-        'it': 'Noi, l\'agenzia digitale per il tuo business',
-        'en': 'We are the digital agency for your business'
+        'it': 'Marketing e comunicazione',
+        'en': 'Marketing and Communication'
+    },
+    subtitle: {
+        'it': 'Cosa proponiamo',
+        'en': 'What we offer'
+    },
+    description: {
+        'it': 'Ti supportiamo per semplificare la tua operatività attraverso un\'assistenza puntuale ed un supporto professionale in tutte le fase del tuo progetto, offrendoti servizi di comunicazione aziendale e marketing strategico su misura.',
+        'en': 'We support you to simplify your operations through timely assistance and professional advisory in all phases of your project, offering you customized corporate communication and strategic marketing services.'
+    },
+    slider: {
+        title: {
+            'it': 'I nostri servizi',
+            'en': 'Our services'
+        },
+        description: {
+            'it': 'Forniamo servizi e consulenza in marketing e comunicazione aziendale attraverso un portafoglio di soluzioni integrate, accompagnando le aziende durante tutto il processo di ridefinizione e sviluppo del business aziendale.',
+            'en': 'We provide services and advisory in marketing and corporate communication through a portfolio of integrated solutions, accompanying companies throughout the process of redefining and developing the business models.'
+        },
+        item01: {
+            title: {
+                'it': 'Comunicazione digitale e tradizionale',
+                'en': 'Digital and traditional communication'
+            },
+            bulletPoints: {
+                item01: {
+                    'it': 'Gestione social media (LinkedIn, Instagram, Facebook, TikTok, ecc.)',
+                    'en': 'Social media management (LinkedInd, Instagram, Facebook, TikTok ...)'
+                },
+                item02: {
+                    'it': 'Sviluppo siti web, mobile app, UX/UI design',
+                    'en': 'Website development, mobile app, UX / UI design'
+                },
+                item03: {
+                    'it': 'Presentazioni aziendali e di business',
+                    'en': 'Corporate and business presentations'
+                },
+                item04: {
+                    'it': 'Pubblicità e promozione',
+                    'en': 'Advertising and promotion'
+                },
+            }
+        },
+        item02: {
+            title: {
+                'it': 'Creazione contenuti',
+                'en': 'Content creation'
+            },
+            bulletPoints: {
+                item01: {
+                    'it': 'Infografiche digitali, motion graphics, caroselli, illustrazioni',
+                    'en': 'Digital infographics, motion graphics, carousels, illustrations'
+                },
+                item02: {
+                    'it': 'Produzione e post-produzione immagini',
+                    'en': 'Production and post-production of images'
+                },
+                item03: {
+                    'it': 'Produzione e post-produzione video, sottotitoli, speakeraggio, montaggio finale',
+                    'en': 'Video production and post-production, subtitles, voiceovers, final editing'
+                },
+                item04: {
+                    'it': 'Preparazione didascalie, storytelling, copywriting',
+                    'en': 'Preparation of captions, storytelling, copywriting'
+                },
+            }
+        },
+        item03: {
+            title: {
+                'it': 'Marketing strategico',
+                'en': 'Strategic marketing'
+            },
+            bulletPoints: {
+                item01: {
+                    'it': 'Brand strategy (analisi e studio del marchio e del modello di business)',
+                    'en': 'Brand strategy (analysis and study of the brand and business model)'
+                },
+                item02: {
+                    'it': 'Digital marketing strategy (strategia dei social media, SEO/SEM, segmentazione clienti)',
+                    'en': 'Digital marketing strategy (social media strategy, SEO / SEM, customer segmentation)'
+                },
+                item03: {
+                    'it': 'Strategic selling (strategia dei canali e del processo di vendita)',
+                    'en': 'Strategic selling (channel strategy and sales process)'
+                }
+            }
+        }
+    },
+    contactForm: {
+        title: {
+            'it': 'Non esitare a contattarci',
+            'en': 'Do not hesitate to contact us'
+        },
+        subtitle: {
+            'it': 'Hai un progetto in mente?',
+            'en': 'Do you have a project in mind?'
+        },
+        fullNameField: {
+            label: {
+                'it': 'Nome e Cognome',
+                'en': 'First Name and Last Name'
+            },
+            errorMessage: {
+                'it': 'Inserisci il tuo Nome e Cognome!',
+                'en': 'Please enter your First Name and Last Name!'
+            }
+        },
+        emailField: {
+            label: {
+                'it': 'Email',
+                'en': 'Email'
+            },
+            errorMessage: {
+                'it': 'Inserisci la tua email!',
+                'en': 'Please enter your email!'
+            }
+        },
+        messageField: {
+            label: {
+                'it': 'Messaggio',
+                'en': 'Message'
+            },
+            errorMessage: {
+                'it': 'inserisci il tuo messaggio!',
+                'en': 'Please enter your message!'
+            }
+        },
+        buttonCTA: {
+            'it': 'Invia richiesta',
+            'en': 'Send request'
+        },
+        thankYouMessage: {
+            'it': 'Grazie per averci contattato! Un membro del nostro team si metterà in contatto con te il prima possibile.',
+            'en': 'Thank you for contacting us! A member from our team will be reaching out to you soon.'
+        },
+        errorPopup: {
+            failedEmailSending: {
+                'it': 'L\'invio dell\'email è fallito!',
+                'en': 'There was an error submitting the request.'
+            }
+        }
     }
 }
