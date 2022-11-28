3ec9f8518a2869c69d72a8ff7aa919c1becce7d4
Fabio Micheletti <fabio.micheletti@forge.srl>
2022-11-28 09:25:38 +0100

    Revert "Updating SMTP configuration to use GMAIL"
    This reverts commit 4d98b86b709224fa1e766bc1215fb38606287cdc.

--- serverless/functions/index.js
+++ serverless/functions/index.js
@@ -29,17 +29,17 @@ const corsSettings = {
  After update, to use the values in the simulator go to `functions` folder and run
  firebase functions:config:get > .runtimeconfig.json
  */
-const smtpConfig = functions.config().smtp
+const registerConfig = functions.config().register
 const recaptchaConfig = functions.config().recaptcha
 
-const smtpServerHost = 'smtp.gmail.com'
+const registerHost = 'authsmtp.securemail.pro'
 const transporter = nodemailer.createTransport({
-    host: smtpServerHost,
+    host: registerHost,
     port: 465,
     secure: true,
     auth: {
-        user: smtpConfig.username,
-        pass: smtpConfig.password
+        user: registerConfig.username,
+        pass: registerConfig.password
     }
 })
 
@@ -106,7 +106,7 @@ exports.contactUs = functions.https.onRequest((request, response) => {
                 }
 
                 transporter.sendMail({
-                    from: smtpConfig.username,
+                    from: registerConfig.username,
                     to: nexxoxpInfoEmail,
                     replyTo: sender,
                     subject: subject,
